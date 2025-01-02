22ef9ad152f34ed13230bb5fe8270a3dca60bb27
Snow <tushar@tusharin.com>
2025-01-02 23:33:43 +0200

    first commit

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [],
+  templateUrl: './housing-location.component.html',
+})
+export class HousingLocationComponent {}
