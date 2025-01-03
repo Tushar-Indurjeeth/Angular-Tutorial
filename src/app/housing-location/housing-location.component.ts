ac0acee6abb649b6204edb4bb09014843cb54f74
Snow <tushar@tusharin.com>
2025-01-03 20:28:38 +0200

    Completed tutorial

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
+import { CommonModule } from '@angular/common';
+import { RouterModule, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterModule],
   templateUrl: './housing-location.component.html',
 })
-export class HousingLocationComponent {}
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
